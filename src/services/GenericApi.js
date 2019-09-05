ecdaafd3ad8f11485d32a9b8f2ebfdf2891a755f
Maxime Despeisse <maxime.despeisse@gmail.com>
2019-09-05 11:42:48 +0200

    Remove logs + Move web.config file to public folder

--- src/services/GenericApi.js
+++ src/services/GenericApi.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const GenericApi = {
 
   get: (url, apiKey, queryParams) => {
-    console.log('apiKey', apiKey);
     return axios.get(url, {
       headers: {
         'Access-Control-Allow-Origin': '*',
@@ -14,7 +13,6 @@ const GenericApi = {
   },
 
   post: (url, apiKey, data, queryParams) => {
-    console.log('apiKey', apiKey);
     return axios.post(url, data, {
       headers: {
         'Access-Control-Allow-Origin': '*',
