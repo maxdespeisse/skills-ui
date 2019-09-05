02c70a4b42fc52100f9c76fe0be564b03459fc6a
Maxime Despeisse <maxime.despeisse@gmail.com>
2019-09-05 16:50:29 +0200

    Add failing test

---
+++ src/components/SkillsTable.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import SkillsTable from './SkillsTable'
+
+test('Should display table header and row', () => {
+  const { getByText } = render(
+    <SkillsTable evaluations={[{ level: 5, skill: 'Scala' }]} />,
+  );
+
+  // .toBeInTheDocument() is an assertion that comes from jest-dom
+  // otherwise you could use .toBeDefined()
+  expect(getByText('Skill')).toBeDefined();
+  expect(getByText('Level')).toBeDefined();
+  expect(getByText('Scala')).toBeDefined();
+  expect(getByText('5')).toBeDefined();
+})
+
+test('Should fail', () => {
+  expect(1).toEqual(2);
+})
